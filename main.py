7b10169bac40609501d7aee3ec8a80d8f9494589
Cosmonaut-itc <felixddhs@outlook.com>
2021-01-23 15:00:16 -0600

    Update class Credential
    Created Credential class

---
+++ main.py
@@ -0,0 +1,7 @@
+
+class Credential:
+    def __init__(self, name, status, area, clearance):
+        self.name = name
+        self.status = status
+        self.area = area
+        self.clearance = clearance
