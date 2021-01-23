8b49670a8794ad3b4eaab65ac879aa33bd975db7
Cosmonaut-itc <felixddhs@outlook.com>
2021-01-23 15:53:31 -0600

    Update main.py
    Erased API

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 response = requests.get("http://api.name-fake.com/random/random.json")
 name = response.json()
 
+nameID = name['name']
 
+print(nameID)
 
 class Credential:
     def __init__(self, name, status, area, clearance):
