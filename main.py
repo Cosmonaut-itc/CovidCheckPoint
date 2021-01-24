7e170b9a07ea4ff169b59a8681dbf270a9d545ea
DavidVelazquez584 <A01632648@itesm.mx>
2021-01-23 18:36:26 -0600

    Se agrego juego inicial y datos a clases

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import names
+import random
 
 class Credential:
     def __init__(self, name, status, area, clearance):
@@ -6,17 +7,41 @@ class Credential:
         self.status = status
         self.area = area
         self.clearance = clearance
+    def __str__(self):
+        return f"name: {self.name}\nstatus: {self.status}\narea: {self.area}\nclearance: {self.clearance}"
 
 class COVID:
-    def __init__(self, date,mask):
-        self.date = date
+    def __init__(self, last_check,mask):
+        self.last_check = last_check
         self.mask = mask
+    def __str__(self):
+        return f"Last Coronavirus Checkout: {self.last_check}\nWearing Mask: {self.mask}"
 
-def assignCredential(name, status, area, clearance):
-    return Credential(name,status,area,clearance)
+def assignCredential():
+    number = random.randint(0,1)
+    if number == 0:
+        worker = "No worker"
+    else:
+        worker = "Worker"
+    number = random.randint(0,4)
+    areas = ["Information Technology", "Admin", "Human Resources", "Accountancy", "Manofacture"]
+    number = random.randint(0,1)
+    if number == 0:
+        bool_clearance = False
+    else:
+        bool_clearance = True
+    return Credential(names.get_full_name(),worker,areas[number], bool_clearance)
 
-def assignCovid(date,mask):
-    return COVID(date,mask)
+def assignCovid():
+    last_check = random.randint(1, 31)
+    number = random.randint(0, 1)
+    if number == 0:
+        mask = "No"
+    else:
+        mask = "Yes"
+    return COVID(last_check ,mask)
+
+def access():
 
 
 
@@ -57,18 +82,26 @@ while True:
                                  
                         
     ''')
-    user_input = input()
+    user_input = int(input("Select an option: "))
+    print ("\n")
     if user_input == 1:
-        for i in range(3):
-            print("Dia" + i+1)
+        for i in range(0,3):
+            print("Dia " + str(i+1))
             if i == 0:
-                for i in range(3):
-                    pass
+                for i in range(0, 3):
+                    person = assignCredential()
+                    print(person)
+                    user_answer = input("Give access to the person? (Y)es, (N)o\n")
+                    if user_answer == "Y":
+                        
+                    else:
+
+                    print("\n")
             elif i == 1:
-                for i in range(4):
+                for i in range(0, 4):
                     pass
             elif i == 2:
-                for i in range(5):
+                for i in range(0, 5):
                     pass
     else:
         break
